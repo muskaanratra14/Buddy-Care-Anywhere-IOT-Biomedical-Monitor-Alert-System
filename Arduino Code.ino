d9e3548f429da5306604a5d6fb1dbd111d6b9221
Muskaan Ratra <64640973+muskaanratra14@users.noreply.github.com>
2020-05-11 17:02:19 +0530

    Add files via upload

---
+++ Arduino Code.ino
@@ -0,0 +1,74 @@
+#include <LiquidCrystal.h>
+//*************************************************
+LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+/********************************************************************/
+int pulsePin = A0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat 
+// these variables are volatile because they are used during the interrupt service routine!
+volatile int BPM;                   // used to hold the pulse rate
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // holds the time between beats, the Inter-Beat Interval
+volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+/********************************************************************/ 
+int swi = 10;
+unsigned long previousMillis=0;
+//*************************************************
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(swi,INPUT_PULLUP);
+  digitalWrite(swi,HIGH);
+  lcd.begin(16, 2);
+  pinMode(blinkPin,OUTPUT);
+  digitalWrite(blinkPin,LOW);
+  lcd.print(" IOT-Biomedical "); //IoT - Biomedical Monitor Alert System (Temp + Hearbeat)
+  lcd.setCursor(0, 1);
+  lcd.print(" Monitor System ");
+  delay(3000);
+  lcd.clear();
+  interruptSetup();       
+  previousMillis = millis();
+  
+}
+//*************************************************
+void loop()
+{
+ 
+   if (QS == true)                       // Quantified Self flag is true when arduino finds a heartbeat
+  {      
+    if(BPM>20 && BPM<200)
+    {
+      //lcd.setCursor(0, 1);lcd.print("P:        ");lcd.setCursor(3, 1);lcd.print(BPM);lcd.setCursor(9, 1);lcd.print("bpm");
+      Serial.print("<");Serial.print("P");Serial.print(BPM);Serial.print(">");
+    }
+    else
+    {
+      BPM=0;
+      //lcd.setCursor(0, 1);lcd.print("P:        ");lcd.setCursor(3, 1);lcd.print('-');lcd.setCursor(9, 1);lcd.print("bpm");
+      Serial.print("<");Serial.print("P");Serial.print(BPM);Serial.print(">");
+    }
+    QS = false;                      // reset the Quantified Self flag for next time    
+  }
+  
+  
+  if(digitalRead(swi)==HIGH)
+  {
+   lcd.setCursor(0, 0);lcd.print("Reading.....     ");
+   lcd.setCursor(0, 1);lcd.print("P:        ");lcd.setCursor(3, 1);lcd.print(BPM);lcd.print(" bpm         ");  
+   if(millis() - previousMillis > 15000){Serial.print("<");Serial.print("U");Serial.print(">");previousMillis = millis();} 
+  }
+  else
+  {
+    // Nothing
+    lcd.setCursor(0, 0);lcd.print("Reading a Button");
+    lcd.setCursor(0, 1);lcd.print("     Press..    ");
+  }
+  
+  
+  
+}
+//*************************************************
+
+
+
---
+++ WiFi Code.ino
@@ -0,0 +1,102 @@
+
+#include <ESP8266WiFi.h>
+//#include <ESP8266WebServer.h>
+
+const char* host = "api.thingspeak.com"; // ThingSpeak domain    
+const int httpPort = 80;
+String ApiKey = "3HGHAE16COXBZPWW";
+String path = "/update?key=" + ApiKey + "&field1=";   
+//String path2 = "&field2=";
+  
+const char* ssid = "AndroidAP";
+const char* pass = "87654321";
+//char temperatureString[6],bpmString[6];
+int b;
+char cmd_arr1[100];
+int cmd_count1;
+int i;
+//*******************************************************************************
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("");
+  pinMode(5,OUTPUT);digitalWrite(5,LOW);
+  WiFi.begin(ssid, pass);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  digitalWrite(5,HIGH);
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.println("- succesfully connected");
+  Serial.println("- starting client");
+}
+//*******************************************************************************
+
+void serial_get_command()
+{
+  char inchar=0;
+  if(Serial.available() > 0)
+  {
+    inchar = Serial.read();    
+    if(inchar == '<')
+    {
+      cmd_count1=0;
+      while(inchar != '>' && cmd_count1<15)
+      {
+        if(Serial.available() > 0)
+        {
+          inchar = Serial.read();
+          cmd_arr1[cmd_count1++] = inchar;
+        }        
+      }
+      if(inchar == '>')
+      {
+        if(cmd_arr1[0]=='P')
+        {
+          cmd_arr1[0]='0';
+          b = atoi(cmd_arr1);
+          Serial.print("BPM: ");
+          Serial.println(b);
+         }
+         else if(cmd_arr1[0]=='U')
+         {
+          cmd_arr1[0]='0';
+          Serial.println("Uploading");
+          send_data();
+          
+         }
+         else
+         {
+          
+         }
+       }
+    }
+  }
+}
+
+//*******************************************************************************
+void send_data()
+{
+  for(i=0;i<=3;i++){digitalWrite(5,LOW);delay(200);digitalWrite(5,HIGH);delay(200);}
+  WiFiClient client;
+  if (!client.connect(host, httpPort))return; 
+  client.print(String("GET ") + path + b + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: keep-alive\r\n\r\n");        
+                      
+  for(i=0;i<=3;i++){digitalWrite(5,LOW);delay(50);digitalWrite(5,HIGH);delay(50);}
+  client.stop();
+}
+//*******************************************************************************
+void loop() 
+{
+ serial_get_command();
+}
+//*******************************************************************************
